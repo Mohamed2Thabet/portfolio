3d23cae69f4d7734f4054c6571b839e349080714
Mohamed2Thabet <mmhh222333666@gmail.com>
2025-12-26 07:42:29 +0200

    Hide Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
     <Education />
     <Experience />
     <RecentProjects />
-    <Testimonials />
+    {/* <Testimonials /> */}
     <Contact />
     <Footer/>
   </div>
